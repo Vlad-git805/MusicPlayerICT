a3fc8588ef81a2fa6178441fdb29d11579cf7b06
Василь Колотюк <vasakolotuk02@gmail.com>
2021-03-27 15:12:07 +0200

    created Playlist entity

---
+++ MusicPlayer.DAL/Entities/Playlist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.DAL.Entities
+{
+    class Playlist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int UserId { get; set; }
+ 
+        public virtual User User { get; set; }
+        public virtual ICollection<Track> Tracks { get; set; }
+    }
+}
