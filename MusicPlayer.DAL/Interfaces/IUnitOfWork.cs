9bb910e5f1761d97a1b41736d838762fa1038154
Віталік <vitalik.baziuk228@gmail.com>
2021-03-27 12:23:23 +0200

    Add DAL

---
+++ MusicPlayer.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.DAL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository</*class*/> /*class*/ { get; }
+        IRepository</*class*/> /*class*/ { get; }
+        void Save();
+    }
+}
