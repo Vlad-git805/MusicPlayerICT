c5bd20675e905d69b696d53caf967a914cab7260
Василь Колотюк <vasakolotuk02@gmail.com>
2021-03-27 15:08:42 +0200

    created Artist entity

---
+++ MusicPlayer.DAL/Entities/Track.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.DAL.Entities
+{
+    class Track
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ArtistId { get; set; }
+        public int AlbumId { get; set; }
+        public string Duration { get; set; }
+        public int NumberOfAuditions { get; set; }
+
+        public virtual Artist Artist { get; set; }
+        public virtual Album Album { get; set; }
+    }
+}
