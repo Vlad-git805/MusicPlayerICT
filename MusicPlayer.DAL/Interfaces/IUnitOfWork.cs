61d7c3190324bc95f3ebd99f87118cc1a4665944
Василь Колотюк <vasakolotuk02@gmail.com>
2021-03-27 16:08:49 +0200

    fix

--- MusicPlayer.DAL/Interfaces/IUnitOfWork.cs
+++ MusicPlayer.DAL/Interfaces/IUnitOfWork.cs
@@ -8,8 +8,8 @@ namespace MusicPlayer.DAL.Interfaces
 {
     public interface IUnitOfWork : IDisposable
     {
-        IRepository</*class*/> /*class*/ { get; }
-        IRepository</*class*/> /*class*/ { get; }
+       // IRepository</*class*/> /*class*/ { get; }
+       // IRepository</*class*/> /*class*/ { get; }
         void Save();
     }
 }
