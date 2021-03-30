82fdae9890f024ba291c1ba934f7163d98a76117
Vlad805 <zhomyruk_ak18@nuwm.edu.ua>
2021-03-30 20:40:18 +0300

    Create audio file metadata analys

---
+++ Client/Model/MetaDataOfTrack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Model
+{
+    public class MetaDataOfTrack
+    {
+        public string Name { get; set; }
+        public string Albom { get; set; }
+        public string Artist { get; set; }
+        public string Ganre { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        public override string ToString()
+        {
+            return Name + " " + Albom + " " + Artist + " " + Ganre + " " + Duration.ToString();
+        }
+    }
+}
